e6a9513d45e7d86ebf53defdbf6b10777335877e
kjwang <kjwang@talend.com>
2020-12-23 15:46:18 +0800

    TUP-29426 Shared Studio: We should not display button "Installation of BPM is required..." on toolbar and task status area. (#3919)
    * TUP-29426 Shared Studio: We should not display button "Installation of
    BPM is required..." on toolbar and task status area.
    https://jira.talendforge.org/browse/TUP-29426

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IUpdateService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IUpdateService.java
@@ -24,5 +24,7 @@ public interface IUpdateService extends IService {
     public boolean syncSharedStudioLibraryInPatch(IProgressMonitor monitor) throws Exception;
     
     public String getSharedStudioMissingPatchVersion();
+    
+    public boolean updateArtifactsFileSha256Hex(IProgressMonitor monitor, String studioArtifactsFileShaCodeHex);
 
 }
--- main/plugins/org.talend.utils/src/main/java/org/talend/utils/string/DigestUtil.java
+++ main/plugins/org.talend.utils/src/main/java/org/talend/utils/string/DigestUtil.java
@@ -1,20 +1,27 @@
-//============================================================================
+// ============================================================================
 //
-//Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
 //
-//This source code is available under agreement available at
-//%InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
-//You should have received a copy of the agreement
-//along with this program; if not, write to Talend SA
-//9 rue Pages 92150 Suresnes, France
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
-//============================================================================
+// ============================================================================
 package org.talend.utils.string;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class DigestUtil {
 
-	public static String sha256Hex(final byte[] data) {
-		return org.apache.commons.codec.digest.DigestUtils.sha256Hex(data);
-	}
+    public static String sha256Hex(final byte[] data) {
+        return org.apache.commons.codec.digest.DigestUtils.sha256Hex(data);
+    }
+
+    public static String sha256Hex(final InputStream inputStream) throws IOException {
+        return org.apache.commons.codec.digest.DigestUtils.sha256Hex(inputStream);
+    }
 }
