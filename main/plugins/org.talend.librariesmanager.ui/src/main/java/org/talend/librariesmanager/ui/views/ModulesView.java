61d8e7308478b7fd49af65e632843669a66fec22
jiezhang-tlnd <40561780+jiezhang-tlnd@users.noreply.github.com>
2021-01-05 14:21:45 +0800

    feat(TUP-29845)Remove useless "Remove" button on module view (#3965)
    https://jira.talendforge.org/browse/TUP-29845

--- main/plugins/org.talend.librariesmanager.ui/src/main/java/org/talend/librariesmanager/ui/views/ModulesView.java
+++ main/plugins/org.talend.librariesmanager.ui/src/main/java/org/talend/librariesmanager/ui/views/ModulesView.java
@@ -31,7 +31,6 @@ import org.talend.librariesmanager.ui.actions.DownloadExternalJarAction;
 import org.talend.librariesmanager.ui.actions.ExportCustomSettingsAction;
 import org.talend.librariesmanager.ui.actions.ImportCustomSettingsAction;
 import org.talend.librariesmanager.ui.actions.ImportExternalJarAction;
-import org.talend.librariesmanager.ui.actions.RemoveExternalJarAction;
 import org.talend.librariesmanager.ui.actions.ShareLibsAction;
 
 /**
@@ -130,8 +129,6 @@ public class ModulesView extends ViewPart {
         manager.add(importSettingAction);
         ExportCustomSettingsAction exportSettingAction = new ExportCustomSettingsAction();
         manager.add(exportSettingAction);
-        RemoveExternalJarAction removeAction = new RemoveExternalJarAction();
-        manager.add(removeAction);
         ImportExternalJarAction importAction = new ImportExternalJarAction();
         manager.add(importAction);
         DownloadExternalJarAction downloadAcion = new DownloadExternalJarAction(this);
