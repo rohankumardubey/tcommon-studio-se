51cb13c6190dfe55b9e2d6f031ab962ee872e5ec
Jane Ding <jding@talend.com>
2021-05-25 18:10:48 +0800

    fix(TUP-30849):Improve build Job performance (#4151)
    * fix(TUP-30849):Improve build Job performance
    https://jira.talendforge.org/browse/TUP-30849
    
    Signed-off-by: jding-tlnd <jding@talend.com>
    
    * fix(TUP-30849):Improve build Job performance
    https://jira.talendforge.org/browse/TUP-30849
    Signed-off-by: jding-tlnd <jding@talend.com>
    
    * fix(TUP-30849):Improve build Job performance
    https://jira.talendforge.org/browse/TUP-30849
    
    Signed-off-by: jding-tlnd <jding@talend.com>
    
    * fix(TUP-30849):Improve build Job performance
    https://jira.talendforge.org/browse/TUP-30849
    
    Signed-off-by: jding-tlnd <jding@talend.com>
    
    * fix(TUP-30849):Improve build Job performance
    https://jira.talendforge.org/browse/TUP-30849
    
    Signed-off-by: jding-tlnd <jding@talend.com>
    
    * fix(TUP-30849):Improve build Job performance
    https://jira.talendforge.org/browse/TUP-30849
    
    Signed-off-by: jding-tlnd <jding@talend.com>
    
    * fix(TUP-30849):Improve build Job performance
    https://jira.talendforge.org/browse/TUP-30849
    
    Signed-off-by: jding-tlnd <jding@talend.com>
    
    * fix(TUP-30849):Improve build Job performance
    https://jira.talendforge.org/browse/TUP-30849
    
    * feat(TUP-31117):Improve performances related to recursive jobs (#4290)
    
    * feat(TUP-31117):Improve performances related to recursive jobs
    https://jira.talendforge.org/browse/TUP-31117
    
    * feat(TUP-31117):Improve performances related to recursive jobs
    https://jira.talendforge.org/browse/TUP-31117

--- main/plugins/org.talend.designer.maven.ui/src/main/java/org/talend/designer/maven/ui/setting/project/page/BuildProjectSettingPage.java
+++ main/plugins/org.talend.designer.maven.ui/src/main/java/org/talend/designer/maven/ui/setting/project/page/BuildProjectSettingPage.java
@@ -12,15 +12,50 @@
 // ============================================================================
 package org.talend.designer.maven.ui.setting.project.page;
 
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.talend.core.runtime.maven.MavenConstants;
 import org.talend.core.runtime.projectsetting.EmptyProjectSettingPage;
+import org.talend.designer.maven.DesignerMavenPlugin;
+import org.talend.designer.maven.ui.i18n.Messages;
 
 /**
  * DOC ggu class global comment. Detailled comment
  */
 public class BuildProjectSettingPage extends EmptyProjectSettingPage {
 
+    private IPreferenceStore preferenceStore;
+
+    private Button allowRecursiveJobsCheckbox;
+
     public BuildProjectSettingPage() {
         super();
     }
 
+    @Override
+    protected String getPreferenceName() {
+        return DesignerMavenPlugin.PLUGIN_ID;
+    }
+
+    @Override
+    protected void createFieldEditors() {
+        Composite parent = getFieldEditorParent();
+        parent.setLayout(new GridLayout());
+        preferenceStore = getPreferenceStore();
+        allowRecursiveJobsCheckbox = new Button(parent, SWT.CHECK);
+        allowRecursiveJobsCheckbox.setText(Messages.getString("BuildProjectSettingPage.allowRecursiveJobs")); //$NON-NLS-1$
+        allowRecursiveJobsCheckbox.setSelection(!preferenceStore.getBoolean(MavenConstants.SKIP_LOOP_DEPENDENCY_CHECK));
+    }
+
+    @Override
+    public boolean performOk() {
+        boolean performOk = super.performOk();
+        if (preferenceStore != null) {
+            preferenceStore.setValue(MavenConstants.SKIP_LOOP_DEPENDENCY_CHECK, !allowRecursiveJobsCheckbox.getSelection());
+        }
+        return performOk;
+    }
 }
