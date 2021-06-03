a5d5c0ef559548a494ed5abdcdcabfd0038aa495
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2021-06-03 16:16:17 +0800

    fix(TUP-31580): replace javascriptengine (#4334)
    * fix(TUP-31580): replace javascriptengine
    
    * fix(TUP-31580): fix test case
    
    * fix(TUP-31580): fix test case

--- test/plugins/org.talend.core.runtime.test/src/org/talend/core/model/utils/ContextParameterUtilsTest.java
+++ test/plugins/org.talend.core.runtime.test/src/org/talend/core/model/utils/ContextParameterUtilsTest.java
@@ -20,8 +20,6 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.junit.Assert;
 import org.junit.Test;
 import org.talend.core.model.context.JobContext;
@@ -30,8 +28,6 @@ import org.talend.core.model.context.JobContextParameter;
 import org.talend.core.model.metadata.types.JavaTypesManager;
 import org.talend.core.model.process.IContext;
 import org.talend.core.model.process.IContextParameter;
-import org.talend.core.model.repository.IRepositoryPrefConstants;
-import org.talend.core.prefs.ITalendCorePrefConstants;
 import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.designer.core.model.utils.emf.talendfile.ContextParameterType;
 import org.talend.designer.core.model.utils.emf.talendfile.ContextType;
@@ -429,6 +425,53 @@ public class ContextParameterUtilsTest {
     	assertTrue(ContextParameterUtils.isDynamic("((String)globalMap.get(\"key\"))"));
     	assertTrue(ContextParameterUtils.isDynamic("\"const\" + ((String)globalMap.get(\"key\")) + \"const\""));
     	assertTrue(ContextParameterUtils.isDynamic("\"const\"+((String)globalMap.get(\"key\"))+\"const\""));
+    }
+
+    @Test
+    public void testIsValidLiteralValue() {
+        String value = "context.var1";
+
+        assertFalse(ContextParameterUtils.isValidLiteralValue(value));
+
+        value = "var1";
+
+        assertFalse(ContextParameterUtils.isValidLiteralValue(value));
+
+        value = "\"var1\"";
+
+        assertTrue(ContextParameterUtils.isValidLiteralValue(value));
+
+        JobContext jc = new JobContext("Default");
+
+        IContextParameter contextParam = new JobContextParameter();
+        contextParam.setName("new1");
+        contextParam.setType(JavaTypesManager.getDefaultJavaType().getId());
+        contextParam.setValue("ab\"c");
+
+        jc.getContextParameterList().add(contextParam);
+
+        contextParam = new JobContextParameter();
+        contextParam.setName("new2");
+        contextParam.setType(JavaTypesManager.getDefaultJavaType().getId());
+        contextParam.setValue("\"ab\"c\"");
+
+        jc.getContextParameterList().add(contextParam);
+
+        value = "context.new1";
+
+        // invoke this to set context variables
+        ContextParameterUtils.convertContext2Literal4AnyVar(value, jc);
+
+        assertTrue(ContextParameterUtils.isValidLiteralValue(value));
+
+        value = "context.new2";
+
+        assertTrue(ContextParameterUtils.isValidLiteralValue(value));
+
+        value = "context.new3";
+
+        assertFalse(ContextParameterUtils.isValidLiteralValue(value));
+
     }
 
 }
