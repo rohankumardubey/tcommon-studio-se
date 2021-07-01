dd1741a90884079fa091b754282e83f5178ff5f6
kjwang <kjwang@talend.com>
2021-07-01 10:23:41 +0800

    Fix TUP-31909 Shared mode for Talend Studio doesn't work   5921 (#4395) (#4399)
    Fix TUP-31909 Shared mode for Talend Studio doesn't work
    https://jira.talendforge.org/browse/TUP-31909

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/runtime/util/SharedStudioUtils.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/runtime/util/SharedStudioUtils.java
@@ -27,45 +27,11 @@ import org.talend.core.GlobalServiceRegister;
 import org.talend.core.model.components.IComponentsFactory;
 import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.core.service.IUpdateService;
-import org.talend.utils.io.FilesUtils;
+
 
 public class SharedStudioUtils {
 
-    public static final String FILE_EXTRA_FEATURE_INDEX = "extra_feature.index"; //$NON-NLS-1$
-
-    public static final String SIGNATURE_FILE_NAME_SUFFIX = ".sig"; //$NON-NLS-1$
     public static final String PROP_DEVMODE = "osgi.dev"; //$NON-NLS-1$
-    
-    public static boolean updateExtraFeatureFile() {
-        File userConfigFolder = new File(Platform.getConfigurationLocation().getURL().getPath());
-        File studioConfigFolder = new File(Platform.getInstallLocation().getURL().getPath(), "configuration");//$NON-NLS-1$
-        if (!userConfigFolder.getAbsolutePath().equals(studioConfigFolder.getAbsolutePath())) {
-            File studioExtraFile = new File(studioConfigFolder, FILE_EXTRA_FEATURE_INDEX);
-            File studioExtraSignFile = new File(studioConfigFolder, FILE_EXTRA_FEATURE_INDEX + SIGNATURE_FILE_NAME_SUFFIX);
-            File userExtraFile = new File(userConfigFolder, FILE_EXTRA_FEATURE_INDEX);
-            File userExtraSignFile = new File(userConfigFolder, FILE_EXTRA_FEATURE_INDEX + SIGNATURE_FILE_NAME_SUFFIX);
-            boolean isNeedUpdate = false;
-            if (!studioExtraSignFile.exists() && userExtraSignFile.exists()) {
-                userExtraSignFile.delete();
-                if (userExtraFile.exists()) {
-                    userExtraFile.delete();
-                }
-                return true;
-            } else if (studioExtraSignFile.exists()) {
-                isNeedUpdate = true;
-            } 
-            if (isNeedUpdate) {
-                try {
-                    FilesUtils.copyFile(studioExtraFile, userExtraFile);
-                    FilesUtils.copyFile(studioExtraSignFile, userExtraSignFile);
-                } catch (IOException ex) {
-                    ExceptionHandler.process(ex);
-                }
-                return true;
-            }
-        }
-        return false;
-    }
     
     public static boolean isSharedStudioMode() {
     	if (isDevEnvironment()) {
