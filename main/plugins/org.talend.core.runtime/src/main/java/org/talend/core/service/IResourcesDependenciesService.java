4590b8456d24676a6988801493ccd8a03625c709
Jane Ding <jding@talend.com>
2021-08-03 10:02:28 +0800

    fix(TUP-32333):Not able to access a repository resource file in TMC (#4466)
    * fix(TUP-32333):Not able to access a repository resource file in TMC
    https://jira.talendforge.org/browse/TUP-32333
    
    * fix(TUP-32333):Not able to access a repository resource file in TMC
    https://jira.talendforge.org/browse/TUP-32333

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IResourcesDependenciesService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/service/IResourcesDependenciesService.java
@@ -1,6 +1,7 @@
 package org.talend.core.service;
 
 import org.eclipse.swt.widgets.Shell;
+import org.talend.core.GlobalServiceRegister;
 import org.talend.core.IService;
 import org.talend.core.model.process.IProcess;
 import org.talend.core.model.repository.IRepositoryViewObject;
@@ -14,6 +15,8 @@ public interface IResourcesDependenciesService extends IService {
 
     public String getResourcePathForContext(IProcess process, String resourceContextValue);
 
+    public String getResourcePathForContext(IProcess process, String resourceContextValue, boolean forceRelative);
+
     public String getResourceItemFilePath(String resourceContextValue);
 
     public void refreshDependencyViewer();
@@ -21,4 +24,12 @@ public interface IResourcesDependenciesService extends IService {
     public void setContextParameterChangeDirtyManually();
 
     public void removeBuildJobCacheForResource(String resourceId);
+
+    public static IResourcesDependenciesService get() {
+        GlobalServiceRegister register = GlobalServiceRegister.getDefault();
+        if (register.isServiceRegistered(IResourcesDependenciesService.class)) {
+            return register.getService(IResourcesDependenciesService.class);
+        }
+        return null;
+    }
 }
