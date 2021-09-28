67a2bac908fbf9b140fe4f19fa6e226900e5263e
Zhiwei Xue <zwxue@talend.com>
2021-09-28 10:30:07 +0800

    fix(TUP-32775):[bug] Studio title top bar version isn't correct after (#4624)
    patch was installed from feature manager

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/ui/IInstalledPatchService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/ui/IInstalledPatchService.java
@@ -12,6 +12,9 @@
 // ============================================================================
 package org.talend.core.ui;
 
+import java.io.IOException;
+
+import org.talend.core.GlobalServiceRegister;
 import org.talend.core.IService;
 import org.talend.core.runtime.maven.MavenArtifact;
 
@@ -24,5 +27,14 @@ public interface IInstalledPatchService extends IService {
 	public String getLatestInstalledVersion(boolean isBar);
 	
     public MavenArtifact getLastIntalledP2Patch();
+
+    void updatePatchInstalled(String installedName, String installedVersion, String comment) throws IOException;
+
+    public static IInstalledPatchService get() {
+        if (GlobalServiceRegister.getDefault().isServiceRegistered(IInstalledPatchService.class)) {
+            return GlobalServiceRegister.getDefault().getService(IInstalledPatchService.class);
+        }
+        return null;
+    }
 
 }
