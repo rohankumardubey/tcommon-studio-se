cae8d53dd40f6aa5ed3b4e9e2bf4a4a8cc9b6eb2
Svitlana Ponomarova <sponomarova@talend.com>
2021-10-15 13:07:14 +0300

    feat(TBD-12137): Synapse GA

--- main/plugins/org.talend.metadata.managment.ui/src/main/java/org/talend/metadata/managment/ui/utils/ExtendedNodeConnectionContextUtils.java
+++ main/plugins/org.talend.metadata.managment.ui/src/main/java/org/talend/metadata/managment/ui/utils/ExtendedNodeConnectionContextUtils.java
@@ -166,7 +166,7 @@ public class ExtendedNodeConnectionContextUtils {
         KnoxUrl,
         KnoxUsername,
         KnoxPassword,
-        KnoxDirectory,
+        KnoxDirectory
     }
 
     static List<IContextParameter> getContextVariables(final String prefixName, Connection conn, Set<IConnParamName> paramSet) {
