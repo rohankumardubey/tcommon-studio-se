51ab061d80ef492b102b7a3896945b5658d18b94
kjwang-talend <kjwang@talend.com>
2021-11-08 15:14:49 +0800

    Make test patch R2022-04v2

--- main/plugins/org.talend.new1.plugin/src/org/talend/new1/plugin/New1PluginLogger.java
+++ main/plugins/org.talend.new1.plugin/src/org/talend/new1/plugin/New1PluginLogger.java
@@ -4,7 +4,7 @@ import org.talend.commons.exception.ExceptionHandler;
 
 public class New1PluginLogger {
     public static void log() {
-        ExceptionHandler.log("R2022-03 the plugins: " + Activator.PLUGIN_ID + " started");
+        ExceptionHandler.log("R2022-03 the plugins: " + Activator.PLUGIN_ID + " started -20211108");
     }
 
 }
