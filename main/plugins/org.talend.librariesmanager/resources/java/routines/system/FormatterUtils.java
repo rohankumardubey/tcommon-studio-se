90558a1e7e2713ddcbc7187ecbd518d396280bf0
tetiana.meronyk <tmeronyk@talend.com>
2022-02-02 00:09:13 +0200

    fix(TDI-47390): tFileInputExcel - Wrong dates recorded when "Less memory consumed for large excel(Event mode)" is selected

--- main/plugins/org.talend.librariesmanager/resources/java/routines/system/FormatterUtils.java
+++ main/plugins/org.talend.librariesmanager/resources/java/routines/system/FormatterUtils.java
@@ -133,7 +133,7 @@ public class FormatterUtils {
             return null;
         }
     }
-    public static Date convert_Date1904(java.util.Date date, boolean isDate1904) {
+    public static Date convert_Date1904(java.util.Date datess) {
                 Calendar c = Calendar.getInstance();
                 c.setTime(date);
                 c.add(Calendar.DATE, 1462);
