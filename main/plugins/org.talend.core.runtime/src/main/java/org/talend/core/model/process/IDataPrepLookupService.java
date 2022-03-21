533dfbae11af78edec9091c5569f5f1c8095dd29
kjwang-talend <kjwang@talend.com>
2022-03-21 13:20:07 +0800

    TUP-34688 Research: Load tck/dataprep join/lookups dependencies

---
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/process/IDataPrepLookupService.java
@@ -0,0 +1,23 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2021 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.model.process;
+
+import java.util.List;
+
+import org.talend.core.IService;
+import org.talend.core.model.general.ModuleNeeded;
+
+public interface IDataPrepLookupService extends IService{
+    List<ModuleNeeded> getLookupModuleNeeded(ModuleNeeded moudule);
+//    List<ModuleNeeded> getLookupModuleNeeded(String family);
+}
