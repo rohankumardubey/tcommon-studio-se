ef71f794b41382c6787fb5c8d94ad1f15b38b77e
Svitlana Ponomarova <sponomarova@talend.com>
2022-04-15 17:38:41 +0300

    fix(TBD-13744): fix

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/components/ComponentUtilities.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/components/ComponentUtilities.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.common.util.EList;
