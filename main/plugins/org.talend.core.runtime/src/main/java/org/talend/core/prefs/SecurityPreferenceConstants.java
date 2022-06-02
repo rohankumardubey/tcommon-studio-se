0142e3306840d2d2b4f03d97a9548b44013dd43c
hcyi <hcyi@talend.com>
2022-06-02 10:52:15 +0800

    feat(TUP-34855):Setup custom JKS for job signature from GUI. (#5305)
    * feat(TUP-34855):Setup custom JKS for job signature from GUI.
    
    * feat(TUP-34855):hide password in mvn command
    
    * feat(TUP-34855):Setup custom JKS for job signature from GUI

---
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/prefs/SecurityPreferenceConstants.java
@@ -0,0 +1,31 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2022 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.core.prefs;
+
+
+/**
+ * created by hcyi on May 17, 2022
+ * Detailled comment
+ *
+ */
+public class SecurityPreferenceConstants {
+
+    public static final String SIGNER_PATH = "signer.path"; //$NON-NLS-1$
+
+    public static final String SIGNER_KEYSTORE_PASSWORD = "signer.keystore.password"; //$NON-NLS-1$
+
+    public static final String SIGNER_KEY_PASSWORD = "signer.key.password"; //$NON-NLS-1$
+
+    public static final String SIGNER_KEY_ALIAS = "signer.key.alias"; //$NON-NLS-1$
+
+}
