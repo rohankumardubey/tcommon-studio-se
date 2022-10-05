3b1ebf43256239fc86ab458c373987f6b570c6b2
apoltavtsev <apoltavtsev@gmail.com>
2022-10-05 20:06:47 +0200

    Add files via upload

---
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/tools/BuildTypeManager.java
@@ -0,0 +1,85 @@
+package org.talend.designer.maven.tools;
+
+import java.lang.reflect.InvocationTargetException;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Display;
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.model.general.Project;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.designer.maven.migration.tasks.CorrectBuildTypeForDIJobMigrationTask;
+import org.talend.designer.maven.migration.tasks.CorrectBuildTypeForDsRestMigrationTask;
+import org.talend.designer.maven.migration.tasks.CorrectBuildTypeForRoutesMigrationTask;
+import org.talend.designer.maven.migration.tasks.CorrectBuildTypeForSOAPServiceJobMigrationTask;
+import org.talend.migration.IProjectMigrationTask;
+import org.talend.repository.ProjectManager;
+import org.talend.repository.RepositoryWorkUnit;
+
+public class BuildTypeManager {
+
+	private IProjectMigrationTask[] syncBuildTypeMigrationTasks = { new CorrectBuildTypeForRoutesMigrationTask(),
+			new CorrectBuildTypeForSOAPServiceJobMigrationTask(), new CorrectBuildTypeForDsRestMigrationTask(),
+			new CorrectBuildTypeForDIJobMigrationTask() };
+
+	public void syncBuildTypes() throws Exception {
+
+		IRunnableWithProgress runnableWithProgress = new IRunnableWithProgress() {
+
+			@Override
+			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+
+				RepositoryWorkUnit<Object> workUnit = new RepositoryWorkUnit<Object>("Synchronize all build types") { //$NON-NLS-1$
+
+					@Override
+					protected void run() {
+						final IWorkspaceRunnable op = new IWorkspaceRunnable() {
+
+							@Override
+							public void run(final IProgressMonitor monitor) throws CoreException {
+								try {
+									syncAllBuildTypesWithProgress(monitor);
+								} catch (Exception e) {
+									ExceptionHandler.process(e);
+								}
+							}
+
+						};
+						IWorkspace workspace = ResourcesPlugin.getWorkspace();
+						try {
+							ISchedulingRule schedulingRule = workspace.getRoot();
+							workspace.run(op, schedulingRule, IWorkspace.AVOID_UPDATE, monitor);
+						} catch (CoreException e) {
+							ExceptionHandler.process(e);
+						}
+					}
+
+				};
+				workUnit.setAvoidUnloadResources(true);
+				ProxyRepositoryFactory.getInstance().executeRepositoryWorkUnit(workUnit);
+			}
+		};
+		new ProgressMonitorDialog(Display.getDefault().getActiveShell()).run(true, true, runnableWithProgress);
+	}
+
+	public void syncAllBuildTypesWithProgress(IProgressMonitor monitor) throws Exception {
+
+		Project project = ProjectManager.getInstance().getCurrentProject();
+
+		final SubProgressMonitor subProgressMonitor = new SubProgressMonitor(monitor, 3);
+
+		for (IProjectMigrationTask task : syncBuildTypeMigrationTasks) {
+			monitor.subTask(task.getDescription());
+			task.execute(project);
+			subProgressMonitor.worked(1);
+		}
+
+		subProgressMonitor.done();
+	}
+}
