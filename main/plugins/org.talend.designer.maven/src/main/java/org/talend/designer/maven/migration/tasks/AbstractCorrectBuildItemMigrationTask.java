6549861a12577133831e969d1a98514ef1dba9e0
apoltavtsev <apoltavtsev@gmail.com>
2022-10-06 08:40:08 +0200

    Update AbstractCorrectBuildItemMigrationTask.java

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/migration/tasks/AbstractCorrectBuildItemMigrationTask.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/migration/tasks/AbstractCorrectBuildItemMigrationTask.java
@@ -2,7 +2,6 @@ package org.talend.designer.maven.migration.tasks;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.talend.commons.exception.PersistenceException;
 import org.talend.commons.runtime.model.emf.EmfHelper;
 import org.talend.core.GlobalServiceRegister;
@@ -21,13 +20,32 @@ import org.talend.designer.maven.migration.common.MigrationReportRecorder;
 import org.talend.repository.model.IProxyRepositoryFactory;
 import org.talend.repository.model.IRepositoryService;
 
-public abstract class AbstractCorrectBuildItemMigrationTask extends AbstractItemMigrationTask {
+public abstract class AbstractCorrectBuildItemMigrationTask extends AbstractItemMigrationTask implements ICorrectBuildTypeMigrationTask {
 
 	protected static final String BUILD_TYPE_PROPERTY = "BUILD_TYPE";
 	protected static final String BUILD_TYPE_STANDALONE = "STANDALONE";
 	protected static final String BUILD_TYPE_OSGI = "OSGI";
 	protected static final String BUILD_TYPE_ROUTE = "ROUTE";
 	protected static final String BUILD_TYPE_ROUTE_MICROSERVICE = "ROUTE_MICROSERVICE";
+	
+	
+//	protected Map<String, String> migratedModels = new HashMap<String, String>();
+//	
+//	protected void clearMigratedModels (String modelName, String migrationTask) {
+//		migratedModels.clear();
+//	}
+//	
+//	protected void storeMigratedModel (String modelName, String migrationTask) {
+//		migratedModels.put(modelName, migrationTask);
+//	}
+//	
+//	protected boolean isModelMigrated (String modelName) {
+//		return migratedModels.containsKey(modelName);
+//	}
+//	
+//	protected String getStoredModelMigraionTask (String modelName) {
+//		return migratedModels.get(modelName);
+//	}
 
 	public void generateReportRecord(MigrationReportRecorder recorder) {
 		MigrationReportHelper.getInstance().addRecorder(recorder);
@@ -90,4 +108,6 @@ public abstract class AbstractCorrectBuildItemMigrationTask extends AbstractItem
 		}
 		return null;
 	}
+	
+	abstract public void clear ();
 }
