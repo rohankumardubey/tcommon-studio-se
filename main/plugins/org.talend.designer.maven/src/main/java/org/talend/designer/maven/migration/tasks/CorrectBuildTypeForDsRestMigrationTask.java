9e026322757c5efdaa1bcf0e08e1774f734a88bc
apoltavtsev <apoltavtsev@gmail.com>
2022-10-06 08:40:57 +0200

    Update CorrectBuildTypeForDsRestMigrationTask.java

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/migration/tasks/CorrectBuildTypeForDsRestMigrationTask.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/migration/tasks/CorrectBuildTypeForDsRestMigrationTask.java
@@ -86,5 +86,10 @@ public class CorrectBuildTypeForDsRestMigrationTask extends AbstractDataServiceJ
 	public String getDescription() {
 		return "Synchronize build types for DS Rest jobs";
 	}
+	
+	@Override
+	public void clear () {
+		
+	}
 
 }
