0c62ab935b7689f1c62988c2a098122eed6b504e
apoltavtsev <apoltavtsev@gmail.com>
2022-10-06 08:41:18 +0200

    Update CorrectBuildTypeForRoutesMigrationTask.java

--- main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/migration/tasks/CorrectBuildTypeForRoutesMigrationTask.java
+++ main/plugins/org.talend.designer.maven/src/main/java/org/talend/designer/maven/migration/tasks/CorrectBuildTypeForRoutesMigrationTask.java
@@ -2,7 +2,9 @@ package org.talend.designer.maven.migration.tasks;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.exception.PersistenceException;
@@ -33,6 +35,8 @@ public class CorrectBuildTypeForRoutesMigrationTask extends AbstractRouteMigrati
 
 	private static final String C_TALEND_JOB = "cTalendJob";
 
+	protected Map<String, String> migratedChildJobs = new HashMap<String, String>();
+
 	/*
 	 * (non-Javadoc)
 	 *
@@ -47,10 +51,11 @@ public class CorrectBuildTypeForRoutesMigrationTask extends AbstractRouteMigrati
 	@SuppressWarnings("unchecked")
 	@Override
 	public ExecutionResult execute(Item item) {
+
 		final ProcessType processType = getProcessType(item);
 
 		boolean modified = false;
-
+		boolean migrationFailure = false;
 		/*
 		 * If no BUILD_TYPE is set then default BUILD_TYPE must be ROUTE
 		 */
@@ -79,6 +84,8 @@ public class CorrectBuildTypeForRoutesMigrationTask extends AbstractRouteMigrati
 		 */
 
 		String currentRouteBuildType = (String) item.getProperty().getAdditionalProperties().get(BUILD_TYPE_PROPERTY);
+
+		String currentRouteID = item.getProperty().getLabel();
 
 		IComponentFilter filter = new NameComponentFilter(C_TALEND_JOB);
 
@@ -115,43 +122,88 @@ public class CorrectBuildTypeForRoutesMigrationTask extends AbstractRouteMigrati
 						Object currentChildBuildType = childItem.getProperty().getAdditionalProperties()
 								.get(BUILD_TYPE_PROPERTY);
 
+						String jobID = childItem.getProperty().getLabel();
+
 						String currentChildBuildTypeStr = (null == currentChildBuildType) ? null
 								: (String) currentChildBuildType;
+						
+						if (BUILD_TYPE_ROUTE.equalsIgnoreCase(currentRouteBuildType)
+								&& BUILD_TYPE_OSGI.equalsIgnoreCase(currentChildBuildTypeStr)) {
+							storeMigratedModel(jobID, currentRouteID);
+						}
 
 						if (BUILD_TYPE_ROUTE.equalsIgnoreCase(currentRouteBuildType)
 								&& !BUILD_TYPE_OSGI.equalsIgnoreCase(currentChildBuildTypeStr)) {
 
-							childItem.getProperty().getAdditionalProperties()
-									.put(TalendProcessArgumentConstant.ARG_BUILD_TYPE, BUILD_TYPE_OSGI);
-
-							generateReportRecord(new MigrationReportRecorder(this,
-									MigrationReportRecorder.MigrationOperationType.MODIFY, childItem, null,
-									"Build Type", currentChildBuildTypeStr, BUILD_TYPE_OSGI));
-
 							try {
-								ProxyRepositoryFactory.getInstance().save(childItemProject, childItem, true);
+								if (isModelMigrated(jobID)) {
+									ExceptionHandler.process(new RuntimeException(
+											"Child Job is called by 2 or more different Routes which have different build types. Build type for child Job ["
+													+ jobID + "] was previously updated to [" + currentChildBuildTypeStr
+													+ "] to be compatible with parent Route ["
+													+ getStoredMigratedModelParentRoute(jobID)
+													+ "] which is not compatible with current parent Route ["
+													+ currentRouteID + "] with build type [" + currentRouteBuildType
+													+ "]. "));
+									migrationFailure = true;
+									return ExecutionResult.FAILURE;
+								} else {
+
+									childItem.getProperty().getAdditionalProperties()
+											.put(TalendProcessArgumentConstant.ARG_BUILD_TYPE, BUILD_TYPE_OSGI);
+
+									generateReportRecord(new MigrationReportRecorder(this,
+											MigrationReportRecorder.MigrationOperationType.MODIFY, childItem, null,
+											"Build Type", currentChildBuildTypeStr, BUILD_TYPE_OSGI));
+
+
+									ProxyRepositoryFactory.getInstance().save(childItemProject, childItem, true);
+								}
+
 							} catch (PersistenceException e) {
 								ExceptionHandler.process(e);
 								return ExecutionResult.FAILURE;
 							}
 						}
+						
+
 
 						if (BUILD_TYPE_ROUTE_MICROSERVICE.equalsIgnoreCase(currentRouteBuildType)
 								&& !BUILD_TYPE_STANDALONE.equalsIgnoreCase(currentChildBuildTypeStr)) {
 
-							childItem.getProperty().getAdditionalProperties()
-									.put(TalendProcessArgumentConstant.ARG_BUILD_TYPE, BUILD_TYPE_STANDALONE);
-
-							generateReportRecord(new MigrationReportRecorder(this,
-									MigrationReportRecorder.MigrationOperationType.MODIFY, childItem, null,
-									"Build Type", currentChildBuildTypeStr, BUILD_TYPE_STANDALONE));
-
 							try {
-								ProxyRepositoryFactory.getInstance().save(childItemProject, childItem, true);
+
+								if (isModelMigrated(jobID)) {
+									ExceptionHandler.process(new RuntimeException(
+											"Child Job is called by 2 or more different Routes which have different build types. Build type for child Job ["
+													+ jobID + "] was previously updated to [" + currentChildBuildTypeStr
+													+ "] to be compatible with parent Route ["
+													+ getStoredMigratedModelParentRoute(jobID)
+													+ "] which is not compatible with current parent Route ["
+													+ currentRouteID + "] with build type [" + currentRouteBuildType
+													+ "]. "));
+									migrationFailure = true;
+									return ExecutionResult.FAILURE;
+								} else {
+
+									childItem.getProperty().getAdditionalProperties()
+											.put(TalendProcessArgumentConstant.ARG_BUILD_TYPE, BUILD_TYPE_STANDALONE);
+
+									generateReportRecord(new MigrationReportRecorder(this,
+											MigrationReportRecorder.MigrationOperationType.MODIFY, childItem, null,
+											"Build Type", currentChildBuildTypeStr, BUILD_TYPE_STANDALONE));
+
+									ProxyRepositoryFactory.getInstance().save(childItemProject, childItem, true);
+								}
 							} catch (PersistenceException e) {
 								ExceptionHandler.process(e);
 								return ExecutionResult.FAILURE;
 							}
+						}
+						
+						if (BUILD_TYPE_ROUTE_MICROSERVICE.equalsIgnoreCase(currentRouteBuildType)
+								&& BUILD_TYPE_STANDALONE.equalsIgnoreCase(currentChildBuildTypeStr)) {
+							storeMigratedModel(jobID, currentRouteID);
 						}
 
 					}
@@ -161,6 +213,10 @@ public class CorrectBuildTypeForRoutesMigrationTask extends AbstractRouteMigrati
 
 		}
 
+		if (migrationFailure) {
+			return ExecutionResult.FAILURE;
+		}
+
 		if (modified) {
 			return ExecutionResult.SUCCESS_NO_ALERT;
 		}
@@ -168,10 +224,31 @@ public class CorrectBuildTypeForRoutesMigrationTask extends AbstractRouteMigrati
 		return ExecutionResult.NOTHING_TO_DO;
 
 	}
-	
+
+	protected void clearMigratedChildJobs() {
+		migratedChildJobs.clear();
+	}
+
+	protected void storeMigratedModel(String jobName, String parentRouteName) {
+		migratedChildJobs.put(jobName, parentRouteName);
+	}
+
+	protected boolean isModelMigrated(String jobName) {
+		return migratedChildJobs.containsKey(jobName);
+	}
+
+	protected String getStoredMigratedModelParentRoute(String jobName) {
+		return migratedChildJobs.get(jobName);
+	}
+
 	@Override
 	public String getDescription() {
 		return "Synchronize build types for Routes (incuding child jobs)";
+	}
+
+	@Override
+	public void clear() {
+		clearMigratedChildJobs();
 	}
 
 }
